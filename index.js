938e1d92270699d8e2b5e9318642566da83f2a0e
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-12-04 22:48:54 +0600

    added movie details API end

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+// const { ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://cineverse_admin1:yCU9dkXu3Pk07dFu@cluster0.8nuar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
@@ -42,6 +43,27 @@ async function run() {
       try {
         const movies = await moviesCollection.find().toArray(); 
         res.json(movies);
+      }
+      catch (error) {
+        console.error("Error retreiving movies:", error);
+        res.status(500).json({ error: "Failed to add movie" });
+      }
+    })
+
+    app.get('/movies/:id', async(req, res) => {
+      try {
+        const { id } = req.params;
+        const movie = await moviesCollection.findOne({ _id: new ObjectId(id) }); 
+
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).json({ error: "Invalid movie ID format" });
+        }
+
+        if (!movie) {
+          return res.status(404).json({ error: "Movie not found" });
+        }
+    
+        res.json(movie);
       }
       catch (error) {
         console.error("Error retreiving movie:", error);

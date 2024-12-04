1ed7c39b561dfcdf683c005073e31c5d39e5ca6a
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-12-04 20:03:45 +0600

    added add movie api end

--- index.js
+++ index.js
@@ -8,6 +8,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cineverse_admin1:yCU9dkXu3Pk07dFu@cluster0.8nuar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const moviesCollection = client.db("cineverse").collection("movies");
+
+    app.post('/movies/add/', async(req, res) => {
+      try {
+        const newMovie = req.body;
+        const result = await moviesCollection.insertOne(newMovie);
+
+        res.status(200).json({ message: "Movie added successfully", movieId: result.insertedId });
+      }
+      catch (error) {
+        console.error("Error adding movie:", error);
+        res.status(500).json({ error: "Failed to add movie" });
+      }
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send("running cineverse")
 })
